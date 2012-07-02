2959f75a694e0b677b3df088f45b5bead10e1115
Zenteno <almighty_123@hotmail.com>
2012-07-02 14:57:18 -0300

    Creo que así debería ser.
    Observaciones pls :)

--- model/graph.py
+++ model/graph.py
@@ -11,3 +11,26 @@ class Graph:
 	#
 	#  PUBLIC METHODS
 	#
+	def complete(self):
+		"""Determina si la matriz (en nuestro caso el grafo) es completa o no
+		Retorna 'True' si es completa, 'False' si no lo es"""
+		dim = self.__matrix.__dimMatrix
+		for i in dim:
+			for j in dim:
+				if j == i:
+					continue
+				elif self.__matrix[i][j] == 0:
+					return False
+		return True
+		
+	def degree(self,node):
+		"""Devuelve el grado de un nodo seleccionado"""
+		dim = self.__matrix.__dimMatrix
+		c=0
+		if nodo>dim:
+			return false
+		for i in dim:
+			for j in dim:
+				if self.__matrix[i][node] ==1:
+					c+=1
+		return c

26f253883ca1bc3e58d6863ea73ee03791090698
Ricardo Cisterna <r.cisternasantos@gmail.com>
2012-07-02 20:01:03 -0400

    Cambios menores

--- model/graph.py
+++ model/graph.py
@@ -1,13 +1,36 @@
-class Graph:
+import matrix
 
+class Graph:
+	"""Clase que representa un grafo en un determinado momento
+	Utiliza la matriz de Adyacencia/Incidencia"""
+	
 	def __init__(self):
 		self.__matrix = Matrix(1)
 		self.__nodes
-
+		self.__bipartite
+		self.__complete
+		self.__directed
+		self.__connected
+	
 	#
 	#  PRIVATE METHODS
 	#
-
+	
+	def __validate_target(self):
+		pass
+	
 	#
 	#  PUBLIC METHODS
 	#
+	
+	def add_node():
+		pass
+	
+	def del_node():
+		pass
+	
+	def change_relation():
+		pass
+	
+	def get_matrix():
+		pass
--- model/matrix.py
+++ model/matrix.py
@@ -1,14 +1,16 @@
 import copy	# Para hacer copias superficiales (shallow copies)
 
 class Matrix:
+	"""Clase que maneja la Matriz de Adyacencia/Incidencia"""
+	
 	def __init__(self, dim):
 		self.__dimMatrix = dim
 		self.__matrix = self.__set_matrix(dim)
-
+	
 	#
 	#  PRIVATE METHODS
 	#
-
+	
 	def __set_matrix(self, dim):
 		"""Para una dimension "dim":
 		Crea una nueva matriz. Todos sus elementos tienen el valor '0'
@@ -19,21 +21,21 @@ class Matrix:
 			for j in range(dim):
 				matrix[i].append(0)
 		return matrix
-
+	
 	#
 	#  PUBLIC METHODS
 	#
-
+	
 	def get_dim(self):
 		"""Retorna una copia 'superficial' (por valor) de la dimension"""
 		dim = copy.copy(self.__dimMatrix)
 		return dim
-
+	
 	def get_matrix(self):
 		"""Retorna una copia 'superficial' (por valor) de la matriz"""
 		matrix = copy.copy(self.__matrix)
 		return matrix
-
+	
 	def add_node(self):
 		"""Agrega los datos de un nodo (nueva fila y columna)
 		Retorna la nueva dimension de la matriz"""
@@ -46,7 +48,7 @@ class Matrix:
 		for i in range(dim):
 			self.__matrix[i].append(0)
 		return dim
-
+	
 	def del_node(self, target):
 		"""Para un indice "target":
 		Elimina los datos de un nodo (fila & columna respectiva)
@@ -57,7 +59,7 @@ class Matrix:
 			self.__matrix[i].pop(target)
 		dim -= 1
 		return dim
-
+	
 	def symmetry(self):
 		"""Determina si la matriz es simetrica o no
 		Retorna 'True' si es simetrica, 'False' si no lo es"""
@@ -69,7 +71,7 @@ class Matrix:
 				if self.__matrix[i][j] != self.__matrix[j][i]:
 					return False
 		return True
-
+	
 	def set_entry(row, col, value):
 		"""Inserta un valor en el elemento (row, col) de la matriz"""
 		self.__matrix[row][col] = value
